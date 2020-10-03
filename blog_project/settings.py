caab6d948a6020d56392eec5cb69308e19b016af
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-10-03 22:17:14 +0600

    feat: add crud for posts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ac)4h*ty8@2=void#$32n#*1o#*ze^yhe)zbgrn4(l8q2kl)fr'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 
     'posts.apps.PostsConfig'
 ]
@@ -120,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+            username='testuser1', password='testuser1'
+        )
+        testuser1.save()
+
+        test_post = Post.objects.create(
+            author=testuser1, title='My blog', body='My blog body'
+        )
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(title, 'My blog')
+        self.assertEqual(body, 'My blog body')
